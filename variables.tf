3f06eb622eaf45d60b7887074f7046bb8366d76a
Sindre Grindvoll <sindre.grindvoll@gmail.com>
2023-06-12 10:25:30 +0200

    feat[terraform]: commented out resources and added troubleshooting output
    Signed-off-by: Sindre Grindvoll <sindre.grindvoll@gmail.com>

--- variables.tf
+++ variables.tf
@@ -76,32 +76,32 @@ variable "access-packages" {
 
 ###   Local Variable - Identity Governance Source Variable transformation
 ############################################################################
-locals {
-  entitlement-catalogs = flatten([                                    # Flattens the nested lists to a list with a depth of 1
-    for catalog in var.access-packages.entitlement_catalogs : catalog # Iterates through all Entitlement Catalogs and creates a list of them
-  ])
-
-  access-packages = flatten([                                      # Flattens the nested lists to a list with a depth of 1
-    for catalog in var.access-packages.entitlement_catalogs : [    # Iterates through all Entitlement Catalogs
-      for ap in catalog.access_packages : merge(ap, {              # Iterates through all Access Packages within each Catalog
-        catalog_key = catalog.display_name                         # Creates a reference key to the Entitlement Catalog
-        key         = "${catalog.display_name}-${ap.display_name}" # Creates a reference key for the Access Package
-      })
-    ]
-  ])
-
-  resources = flatten([                                                                              # Flattens the nested lists to a list with a depth of 1
-    for catalog in var.access-packages.entitlement_catalogs : [                                      # Iterates through all Entitlement Catalogs
-      for ap in catalog.access_packages : [                                                          # Iterates through all Access Packages within each Catalog
-        for resource in ap.resources : merge(resource, {                                             # Iterates through all Resources within each Access Package, within each Catalog
-          catalog_key        = catalog.display_name                                                  # Creates a reference key to the Entitlement Catalog
-          access_package_key = "${catalog.display_name}-${ap.display_name}"                          # Creates a reference key to the Access Package
-          key                = "${catalog.display_name}-${ap.display_name}-${resource.display_name}" # Creates a reference key to the Resource Associations
-        })
-      ]
-    ]
-  ])
-}
+#locals {
+#  entitlement-catalogs = flatten([                                    # Flattens the nested lists to a list with a depth of 1
+#    for catalog in var.access-packages.entitlement_catalogs : catalog # Iterates through all Entitlement Catalogs and creates a list of them
+#  ])
+#
+#  access-packages = flatten([                                      # Flattens the nested lists to a list with a depth of 1
+#    for catalog in var.access-packages.entitlement_catalogs : [    # Iterates through all Entitlement Catalogs
+#      for ap in catalog.access_packages : merge(ap, {              # Iterates through all Access Packages within each Catalog
+#        catalog_key = catalog.display_name                         # Creates a reference key to the Entitlement Catalog
+#        key         = "${catalog.display_name}-${ap.display_name}" # Creates a reference key for the Access Package
+#      })
+#    ]
+#  ])
+#
+#  resources = flatten([                                                                              # Flattens the nested lists to a list with a depth of 1
+#    for catalog in var.access-packages.entitlement_catalogs : [                                      # Iterates through all Entitlement Catalogs
+#      for ap in catalog.access_packages : [                                                          # Iterates through all Access Packages within each Catalog
+#        for resource in ap.resources : merge(resource, {                                             # Iterates through all Resources within each Access Package, within each Catalog
+#          catalog_key        = catalog.display_name                                                  # Creates a reference key to the Entitlement Catalog
+#          access_package_key = "${catalog.display_name}-${ap.display_name}"                          # Creates a reference key to the Access Package
+#          key                = "${catalog.display_name}-${ap.display_name}-${resource.display_name}" # Creates a reference key to the Resource Associations
+#        })
+#      ]
+#    ]
+#  ])
+#}
 
 output "var-input" {
   value = var.access-packages
