9569e9d87465d616fa5101ef0f62d0fb32ce4060
Sindre Grindvoll - Amesto Fortytwo <sindre.grindvoll@amesto.no>
2024-02-20 08:54:03 +0100

    fix: created the specific catalog and access package resource association keys in the source local to ensure unique keys, no duplicates and more readability
    Signed-off-by: Sindre Grindvoll - Amesto Fortytwo <sindre.grindvoll@amesto.no>

--- variables.tf
+++ variables.tf
@@ -91,13 +91,14 @@ locals {
     ]
   ])
 
-  resources = flatten([                                                                              # Flattens the nested lists to a list with a depth of 1
-    for catalog in var.entitlement_catalogs : [                                                      # Iterates through all Entitlement Catalogs
-      for ap in catalog.access_packages : [                                                          # Iterates through all Access Packages within each Catalog
-        for resource in ap.resources : merge(resource, {                                             # Iterates through all Resources within each Access Package, within each Catalog
-          catalog_key        = catalog.display_name                                                  # Creates a reference key to the Entitlement Catalog
-          access_package_key = "${catalog.display_name}-${ap.display_name}"                          # Creates a reference key to the Access Package
-          key                = "${catalog.display_name}-${ap.display_name}-${resource.display_name}" # Creates a reference key to the Resource Associations
+  resources = flatten([                                                                                                   # Flattens the nested lists to a list with a depth of 1
+    for catalog in var.entitlement_catalogs : [                                                                           # Iterates through all Entitlement Catalogs
+      for ap in catalog.access_packages : [                                                                               # Iterates through all Access Packages within each Catalog
+        for resource in ap.resources : merge(resource, {                                                                  # Iterates through all Resources within each Access Package, within each Catalog
+          catalog_key                             = catalog.display_name                                                  # Creates a reference key to the Entitlement Catalog
+          access_package_key                      = "${catalog.display_name}-${ap.display_name}"                          # Creates a reference key to the Access Package
+          access_package_resource_association_key = "${catalog.display_name}-${ap.display_name}-${resource.display_name}" # Creates a reference key for the Access Package Resource Associations
+          catalog_resource_association_key        = "${catalog.display_name}-${resource.display_name}"                    # Creates a reference key to be used for the Catalog Resource Associations
         })
       ]
     ]
