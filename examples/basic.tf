4abbb09fcaa2bea714445bed90a3a5345c6e21c9
Sindre Grindvoll - Amesto Fortytwo <sindre.grindvoll@amesto.no>
2024-05-31 13:21:41 +0200

    doc: updated reference URL
    Signed-off-by: Sindre Grindvoll - Amesto Fortytwo <sindre.grindvoll@amesto.no>

--- examples/basic.tf
+++ examples/basic.tf
@@ -1,6 +1,7 @@
 # This example contains a typical, basic deployment of an Entitlement Catalog, with an Access Package, an Assignment Policy, and AzureAD Groups used as resources.
 # Most of the parameters and inputs are left to their default values, as they are typically the correct values in a common deployment.
-# Refer to the [documentation](https://github.com/amestofortytwo/terraform-azuread-entitlement-management) for all available input parameters.
+# Refer to the [documentation](https://github.com/fortytwoservices/terraform-azuread-entitlement-management) for all available input parameters.
+
 
 ###   Azure AD Groups
 ########################
