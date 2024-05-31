60f14f2760305866fa35c237a959861a8f6af6ce
Sindre Grindvoll - Amesto Fortytwo <sindre.grindvoll@amesto.no>
2024-05-31 12:48:36 +0200

    feat!: removed commented lines from old primary approvers logic. The new input method and logic for primary approvers is a breaking change as it requires changes to the module input. These are reflected in the basic and advanced examples.
    Signed-off-by: Sindre Grindvoll - Amesto Fortytwo <sindre.grindvoll@amesto.no>

--- variables.tf
+++ variables.tf
@@ -31,8 +31,6 @@ variable "entitlement_catalogs" {
       approver_justification_required     = optional(bool, false) # Whether an approver must provide a justification for their decision. Defaults to "false"
       alternative_approval_enabled        = optional(bool, false) # Whether alternative approvers are enabled. Defaults to false
       enable_alternative_approval_in_days = optional(number)      # Number of days before the request is forwarded to alternative approvers
-      #      primary_approver_subject_type       = optional(string)      # Specifies the type of user. singleUser, groupMembers, connectedOrganizationMembers, requestorManager, internalSponsors, or externalSponsors
-      #      primary_approver_object_id          = optional(string)      # Object ID of the Primary Approver(s)
 
       primary_approvers = optional(list(object({ # A list of objects, with one object for each Primary Approver
         subject_type = string                    # Specifies the type of user. singleUser, groupMembers, connectedOrganizationMembers, requestorManager, internalSponsors, or externalSponsors
