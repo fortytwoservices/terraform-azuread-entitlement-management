2432fb67e2aa277e1f35fbd21519f6f225556797
Ketil Gjerde <ketil.gjerde@fortytwo.io>
2025-10-09 13:11:57 +0200

    chore: update with code
    Signed-off-by: Ketil Gjerde <ketil.gjerde@fortytwo.io>

--- main.tf
+++ main.tf
@@ -181,7 +181,7 @@ data "msgraph_resource" "resource_access_package_catalog_resources" {
   for_each         = { for resource in local.resources : resource.access_package_resource_association_key => resource if resource.resource_origin_system == "AadApplication" }
   url              = "/identityGovernance/entitlementManagement/catalogs/${azuread_access_package_catalog.entitlement-catalogs[each.value.catalog_key].id}/resources"
   query_parameters = {
-    "$filter" = ["(originSystem eq %27AadApplication%27 and resource/id eq %27${each.value.resource_origin_id}%27)"]
+    "$filter" = ["(originSystem eq 'AadApplication' and resource/id eq '${each.value.resource_origin_id}')"]
     "$expand" = ["scopes"]
   }
   response_export_values = {
@@ -204,7 +204,7 @@ data "msgraph_resource" "resource_access_package_catalog_resource_roles" {
   for_each = { for resource in local.resources : resource.access_package_resource_association_key => resource if resource.resource_origin_system == "AadApplication" }
   url      = "/identityGovernance/entitlementManagement/catalogs/${azuread_access_package_catalog.entitlement-catalogs[each.value.catalog_key].id}/resourceRoles"
   query_parameters = {
-    "$filter" = ["(originSystem eq %27AadApplication%27 and resource/id eq %27${each.value.resource_origin_id}%27)"]
+    "$filter" = ["(originSystem eq 'AadApplication' and resource/id eq '${each.value.resource_origin_id}')"]
     "$expand" = ["resource"]
   }
   response_export_values = {
